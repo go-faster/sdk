c7ed988d871b7ccc9046b5c51f8485db61a610ed
Aleksandr Razumov <ernado@ya.ru>
2023-04-20 08:48:44 +0300

    refactor: simplify zctx

--- zctx/zctx.go
+++ zctx/zctx.go
@@ -12,20 +12,10 @@ type key struct{}
 
 var _nop = zap.NewNop()
 
-type logger struct {
-	base *zap.Logger
-
-	// Not appending span, trace to base *zap.Logger so fields won't duplicate.
-	//
-	// See https://github.com/uber-go/zap/issues/765
-	span  zap.Field
-	trace zap.Field
-}
-
-func from(ctx context.Context) logger {
-	v, ok := ctx.Value(key{}).(logger)
+func from(ctx context.Context) *zap.Logger {
+	v, ok := ctx.Value(key{}).(*zap.Logger)
 	if !ok {
-		return logger{base: _nop}
+		return _nop
 	}
 	return v
 }
@@ -36,34 +26,28 @@ func From(ctx context.Context) *zap.Logger {
 
 	// Add or update trace and span IDs if available.
 	if s := trace.SpanContextFromContext(ctx); s.IsValid() {
-		const kTrace, kSpan = "trace_id", "span_id"
-
-		// Hex-encoded lowercase as per OTEL log data model.
-		traceID, spanID := s.TraceID().String(), s.SpanID().String()
-		if lg.trace.Key == kTrace {
-			lg.trace.String = traceID
-		} else {
-			lg.trace = zap.String(kTrace, traceID)
-		}
-		if lg.span.Key == kSpan {
-			lg.span.String = spanID
-		} else {
-			lg.span = zap.String(kSpan, spanID)
-		}
-		return lg.base.With(lg.trace, lg.span)
+		return lg.With(
+			// Hex-encoded lowercase as per OTEL log data model.
+			zap.String("trace_id", s.TraceID().String()),
+			zap.String("span_id", s.SpanID().String()),
+		)
 	}
 
-	return lg.base
+	return lg
 }
 
 // With returns new context with provided zap fields.
+//
+// The span and trace IDs must not be added to the base logger because zap
+// can't update or replace fields.
 func With(ctx context.Context, fields ...zap.Field) context.Context {
-	lg := from(ctx)
-	lg.base = lg.base.With(fields...)
-	return context.WithValue(ctx, key{}, lg)
+	return context.WithValue(ctx, key{}, from(ctx).With(fields...))
 }
 
-// Base initializes root logger for using as base context.
+// Base initializes root logger for using as base context. Should be done and early.
+//
+// The span and trace IDs must not be added to the base logger because zap
+// can't update or replace fields.
 func Base(ctx context.Context, lg *zap.Logger) context.Context {
-	return context.WithValue(ctx, key{}, logger{base: lg})
+	return context.WithValue(ctx, key{}, lg)
 }
